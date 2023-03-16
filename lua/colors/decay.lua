de25415f3eb6328c788b33c9c91df368576d9a69
Zhongmin Lin <zhongmin.lin3@gmail.com>
2023-03-16 19:49:00 -0400

    Update colorschemes

---
+++ lua/colors/decay.lua
@@ -0,0 +1,18 @@
+return {
+  base00 = "#101419",
+  base01 = "#1a1e23",
+  base02 = "#21262e",
+  base03 = "#485263",
+  base04 = "#b6beca",
+  base05 = "#cfd2d7",
+  base06 = "#d4d7dc",
+  base07 = "#dee1e6",
+  base08 = "#e05f65",
+  base09 = "#e89777",
+  base0A = "#f1cf8a",
+  base0B = "#78dba9",
+  base0C = "#91c7e7",
+  base0D = "#70a5eb",
+  base0E = "#c68aee",
+  base0F = "#f6c9d4",
+}
