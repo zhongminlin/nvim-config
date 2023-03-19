6bf92510a5167d6aeb54e19982f97f11f66d3014
Zhongmin Lin <zhongmin.lin3@gmail.com>
2023-03-19 17:17:38 -0400

    Minor edits

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -18,12 +18,7 @@ return {
     },
     {
         "lukas-reineke/indent-blankline.nvim",
-        opts = {
-                show_current_context = true,
-                show_current_context_start = true,
-        }, 
     },
-
     { 
         'echasnovski/mini.pairs', 
         version = false,
