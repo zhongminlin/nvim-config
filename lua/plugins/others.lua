47450a0b213ac01bfd3921f46919ca1fc1dbe91e
Zhongmin Lin <zhongmin.lin3@gmail.com>
2023-08-02 21:10:51 -0400

    added treesitter
    trying ts in windows

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -41,8 +41,20 @@ return {
 	   'echasnovski/mini.statusline',
 		version = false,
 		config = function()
-	           require('mini.statusline').setup()
+            require('mini.statusline').setup()
 		end,
 	},
-    'morandi-theme/morandi.nvim',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup(
+                {
+                    ensure_installed = { "bash", "lua", "vim", "python", "javascript", "html" },
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                }
+            )
+        end,
+    },
 }
