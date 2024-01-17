777f79b3927372c83eec4b465d1820e874eb6f04
Zhongmin Lin <zhongmin.lin3@gmail.com>
2024-01-17 15:22:28 -0500

    Added treesitter and header art

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -61,4 +61,17 @@ return {
         event = 'BufReadPre',
         opts = { options = {'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals'} },
     },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup(
+                {
+                    ensure_installed = { "bash", "lua", "vim", "python", "javascript", "html"},
+                    highlight = { enable = true },
+                    indent = { enable = true},
+                }
+            )    
+        end,
+    },
 }
