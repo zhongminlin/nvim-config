39cea5edd515d36f8d9ac8e337ad6d196979aa3e
Zhongmin Lin <zhongmin.lin3@gmail.com>
2024-03-31 23:20:22 -0400

    added traditional colors

---
+++ lua/colors/traditional.lua
@@ -0,0 +1,38 @@
+-- dark
+return {
+  base00 = "#262729",
+  base01 = "#2f3243",
+  base02 = "#343041",
+  base03 = "#7a7b78",
+  base04 = "#a8a9a2",
+  base05 = "#d3d2c9",
+  base06 = "#eae5e4",
+  base07 = "#f5f5f5",
+  base08 = "#a75d68",
+  base09 = "#f5ca67",
+  base0A = "#ebd79c",
+  base0B = "#6b8650",
+  base0C = "#9ab5c3",
+  base0D = "#106898",
+  base0E = "#a878af",
+  base0F = "#8c6951",
+}
+-- light
+-- return {
+--   base00 = "#ececea",
+--   base01 = "#dadad8",
+--   base02 = "#939391",
+--   base03 = "#656565",
+--   base04 = "#424242",
+--   base05 = "#20233c",
+--   base06 = "#0e101b",
+--   base07 = "#05060a",
+--   base08 = "#",
+--   base09 = "#",
+--   base0A = "#",
+--   base0B = "#",
+--   base0C = "#5aa4af",
+--   base0D = "#105b7b",
+--   base0E = "#",
+--   base0F = "#",
+-- }
